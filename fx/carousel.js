4a7b2326e08c09516975ff93f30e3b9f858e66ec
Ricardo Delgado <ricdelgado80@gmail.com>
2018-08-08 22:36:31 -0400

    change carousel button icons in curiosity post

--- fx/carousel.js
+++ fx/carousel.js
@@ -4,9 +4,55 @@ import '../lib/react-dom.js'
 
 class CarouselWidget extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.state = { button: 'caret' }
+    this.carousel = document.querySelector('fx-carousel')
+  }
+
+
+  componentDidUpdate(prevState) {
+    if(this.state.button !== prevState.button) {
+      this.carousel.button = this.state.button
+    }
+  }
+
+
+  changeButton(type) {
+    this.setState(prevState => ({
+      button: type
+    }))
+  }
+
+
   render() {
     return (
-      <h1> test!!! </h1>
+      <ul className="button-styles">
+        <li
+          className="arrow"
+          onClick={() => this.changeButton('arrow')}
+        >
+          Arrow
+        </li>
+        <li
+          className="circle-arrow"
+          onClick={() => this.changeButton('circle-arrow')}
+        >
+          Arrow in Circle
+        </li>
+        <li
+          className="caret"
+          onClick={() => this.changeButton('caret')}
+        >
+          Caret
+        </li>
+        <li
+          className="circle"
+          onClick={() => this.changeButton('circle')}
+        >
+          Caret in Circle
+        </li>
+      </ul>
     )
   }
 
